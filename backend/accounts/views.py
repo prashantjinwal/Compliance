cf8bca717bbc4c3ee1a8a9c4e4bef3f29cedd540
Sourabh-Thakur12 <sourabhthakur0423@gmail.com>
2026-04-17 17:43:04 +0530

    created auth api

---
+++ backend/accounts/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.views import APIView
+from drf_spectacular.utils import extend_schema
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+# Create your views here.
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    @extend_schema(
+        request=RegisterSerializer,
+        responses={201: UserSerializer},
+        description="Register a new user with organization"
+    )
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        return Response(
+            UserSerializer(user).data,
+            status=status.HTTP_201_CREATED
+        )
+
+
+
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    @extend_schema(
+        request=LoginSerializer,
+        responses={
+            200: {
+                "type": "object",
+                "properties": {
+                    "access": {"type": "string"},
+                    "refresh": {"type": "string"},
+                }
+            }
+        },
+        description="Login and get JWT tokens"
+    )
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data
+
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            "access": str(refresh.access_token),
+            "refresh": str(refresh),
+        })
+
+
+
+class MeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @extend_schema(
+        responses={200: UserSerializer},
+        description="Get current authenticated user"
+    )
+    def get(self, request):
+        return Response(UserSerializer(request.user).data)
